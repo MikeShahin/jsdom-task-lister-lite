2e7c34c25a173db43231c86f549a2ad0f9d45457
MikeShahin <gobias101@gmail.com>
2021-02-07 15:02:21 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const submit = document.getElementById("submit")
+  
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const taskList = document.getElementById("tasks")
+    let task = document.getElementById("new-task-description").value
+    let li = document.createElement("li")
+    li.innerText = task
+    taskList.appendChild(li)
+    console.log(li)
+  })
+
 });
